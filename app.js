c68c9ccea7c1262f68af7943b500f657780c9998
sensone <sensone@MacBook-Pro-sensone.local>
2015-03-27 22:16:01 +0300

    change emit to broadcast.emit

--- app.js
+++ app.js
@@ -14,43 +14,43 @@ io.on('connection', function (socket) {
   // listening remote controll
   socket.on('remote:right', function (data) {
     console.log('remote:right', data);
-    socket.emit('presentation:right', data);
+    socket.broadcast.emit('presentation:right', data);
   });
 
   socket.on('remote:left', function (data) {
     console.log('remote:left', data);
-    socket.emit('presentation:left', data);
+    socket.broadcast.emit('presentation:left', data);
   });
 
   socket.on('remote:up', function (data) {
     console.log('remote:up', data);
-    socket.emit('presentation:up', data);
+    socket.broadcast.emit('presentation:up', data);
   });
 
   socket.on('remote:down', function (data) {
     console.log('remote:down', data);
-    socket.emit('presentation:down', data);
+    socket.broadcast.emit('presentation:down', data);
   });
 
   // listening presentation
   socket.on('presentation:right', function (data) {
     console.log('presentation:right', data);
-    socket.emit('remote:right', data);
+    socket.broadcast.emit('remote:right', data);
   });
 
   socket.on('presentation:left', function (data) {
     console.log('presentation:left', data);
-    socket.emit('remote:left', data);
+    socket.broadcast.emit('remote:left', data);
   });
 
   socket.on('presentation:up', function (data) {
     console.log('presentation:up', data);
-    socket.emit('remote:up', data);
+    socket.broadcast.emit('remote:up', data);
   });
 
   socket.on('presentation:down', function (data) {
     console.log('presentation:down', data);
-    socket.emit('remote:down', data);
+    socket.broadcast.emit('remote:down', data);
   });
 
 });
