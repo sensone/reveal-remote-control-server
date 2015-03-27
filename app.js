f4e457e0ab9611daed61d6fde3e7cfa985930986
sensone <sensone@MacBook-Pro-sensone.local>
2015-03-27 21:14:00 +0300

    create API for sockets

--- app.js
+++ app.js
@@ -11,8 +11,46 @@ server.listen(port, function () {
 io.on('connection', function (socket) {
   console.log('connection');
 
-  socket.on('next', function () {
-    console.log('next')
-    socket.broadcast.emit('next', {test: 'test'});
+  // listening remote controll
+  socket.on('remote:right', function (data) {
+    console.log('remote:right', data);
+    socket.emit('presentation:right', data);
   });
+
+  socket.on('remote:left', function (data) {
+    console.log('remote:left', data);
+    socket.emit('presentation:left', data);
+  });
+
+  socket.on('remote:up', function (data) {
+    console.log('remote:up', data);
+    socket.emit('presentation:up', data);
+  });
+
+  socket.on('remote:down', function (data) {
+    console.log('remote:down', data);
+    socket.emit('presentation:down', data);
+  });
+
+  // listening presentation
+  socket.on('presentation:right', function (data) {
+    console.log('presentation:right', data);
+    socket.emit('remote:right', data);
+  });
+
+  socket.on('presentation:left', function (data) {
+    console.log('presentation:left', data);
+    socket.emit('remote:left', data);
+  });
+
+  socket.on('presentation:up', function (data) {
+    console.log('presentation:up', data);
+    socket.emit('remote:up', data);
+  });
+
+  socket.on('presentation:down', function (data) {
+    console.log('presentation:down', data);
+    socket.emit('remote:down', data);
+  });
+
 });
